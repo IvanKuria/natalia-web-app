990f60d8bc4e34a7d58e9733305b4c100aaa2f16
Ivan Kuria <ivankuria7@gmail.com>
2025-03-12 02:57:28 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+  const [firstGif, setFirstGif] = useState("src/assets/Cat Pop Sticker.gif");
+  const [defaultText, setDefaultText] = useState("Are you free this Sunday?")
+
+  const catGifArray = [
+    "src/assets/Happy Video Game Sticker.gif",
+    "src/assets/Happy Dance Sticker.gif",
+    "src/assets/Cat Dancing Sticker.gif",
+  ]
+
+  function handleYesClick() {
+    let i = Math.floor(Math.random() * catGifArray.length);
+    setFirstGif(catGifArray[i]);
+    setDefaultText("Yay see you then!");
+  }
+
+  return (
+    <div className="component-wrapper">
+      <img src={firstGif} alt="Cute Cat GIF" />
+      <span>{defaultText}</span>
+      <div className="button-container">
+        <button className="button-1" onClick={handleYesClick} role="button">
+          Yes
+        </button>
+        <button className="button-2" role="button">
+          No
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
